d2976376493e6afaf3cd4d4961435ba5ccbf7211
Shouko0522 <iamshk0522@gmail.com>
2020-05-24 13:37:10 +0900

    create db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,16 @@
 require 'faker'
 
-email.create(adress_from:"",adress_to:"nii@gmail.com",date:"123456")
-email.create(adress_from:"",adress_to:"nii@gmail.com",date:"123456")
-email.create(adress_from:"",adress_to:"nii@gmail.com",date:"123456")
-email.create(adress_from:"",adress_to:"nii@gmail.com",date:"123456")
-email.create(adress_from:"",adress_to:"nii@gmail.com",date:"123456")
-email.create(adress_from:"",adress_to:"nii@gmail.com",date:"123456")
+email.create(address_from:"",address_to:"nii@gmail.com",date:"123456")
+
 
 
 
 puts 'Creating 100 fake email...'
 100.times do
   email = Email.create!(
-    adress_from: Faker::Name.name,
-    adress_to: Faker::Internet.email,
-    date: "123456"
+    address_from: Faker::Internet.email,
+    address_to: Faker::Internet.email,
+    date: Faker::DateTime
   )
   email.save!
 
