dc60302eb6134ec4df5e133766b7cb63a19c4f4b
Shouko0522 <iamshk0522@gmail.com>
2020-05-30 20:46:26 +0900

    create filter to get emails from data

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -10,15 +10,30 @@ for(var i = 0; i < 100; i++){
   })
 }
 
-console.log(ALL_DATA)
+console.log(ALL_DATA);
 
-// $(function() {
-//   $('input[name="daterange"]').daterangepicker({
-//     opens: 'left'
-//     }, function(start, end, label) {
-//       console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-//     });
-//   });
+let filtered = ALL_DATA.slice();
+
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+    }, function(start, end, label) {
+      filtered = ALL_DATA.filter(data => data.date > start && data.date < end);
+
+      filtered.forEach((item) => {
+        document.getElementById("from").innerHTML = item.sender;
+        document.getElementById("to").innerHTML = item.receiver;
+        document.getElementById("subject").innerHTML = item.subject;
+        document.getElementById("date").innerHTML = item.date;
+      })
+
+      console.log(filtered)
+      // console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+    // const emails = select * from email ALL_DATA between start and end;
+  });
+
+document.getElementById("total-email").innerHTML = filtered.length;
 
 
-// document.getElementById("totalemails").innerHTML; = email;
+
